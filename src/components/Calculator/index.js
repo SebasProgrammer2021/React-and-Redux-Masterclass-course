6e6de17b85eea2aaeb18a9413533f56e4ebc7cd3
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2023-10-16 13:30:23 -0500

    update

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import './Calculator.css';
+import ControlPanel from "../ControlPanel";
+import Digits from "../Digits";
+import Display from "../Display";
+import History from "../History";
+import Operators from "../Operators";
+
+export class Calculator extends Component {
+    render() {
+        return (
+            <main className="react-calculator">
+                <Display />
+                <ControlPanel />
+                <Digits />
+                <Operators />
+                < />
+            </main>
+        )
+    }
+}
+
+export default Calculator;
